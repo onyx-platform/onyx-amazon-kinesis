11d101edf2403a299c148015a2ab95599fe210b0
Lucas Bradstreet <lucasbradstreet@gmail.com>
2017-06-04 18:56:53 -0700

    Add missing input test, increase allowed time for CI.

--- test/onyx/plugin/input_test.clj
+++ test/onyx/plugin/input_test.clj
@@ -1,4 +1,4 @@
-(ns onyx.plugin.input-fault-tolerance-test
+(ns onyx.plugin.input-test
   (:require [clojure.test :refer [deftest is]]
             [com.stuartsierra.component :as component]
             [onyx.test-helper :refer [with-test-env]]
@@ -12,21 +12,6 @@
 
 (def n-partitions 2)
 
-(def test-state (atom nil))
-
-(defn update-atom! [event window trigger {:keys [lower-bound upper-bound event-type] :as state-event} extent-state]
-  (reset! test-state extent-state))
-
-(def read-crash
-  {:lifecycle/after-batch
-   (fn [event lifecycle]
-     (when (and (not (empty? (:onyx.core/batch event)))
-                (zero? (rand-int 70)))
-       (println "Cycle job.")
-       (throw (ex-info "Restartable" {:restartable? true})))
-     {})
-   :lifecycle/handle-exception (constantly :restart)})
-
 (defn build-job [stream-name region batch-size batch-timeout]
   (let [batch-settings {:onyx/batch-size batch-size :onyx/batch-timeout batch-timeout}
         base-job (merge {:workflow [[:read-messages :identity]
@@ -35,24 +20,15 @@
                                            :onyx/fn :clojure.core/identity
                                            :onyx/type :function}
                                           batch-settings)]
-                         :lifecycles [{:lifecycle/task :read-messages
-                                       :lifecycle/calls ::read-crash}]
-                         :windows [{:window/id :collect-segments
-                                    :window/task :identity
-                                    :window/type :global
-                                    :window/aggregation :onyx.windowing.aggregation/conj}]
-                         :triggers [{:trigger/window-id :collect-segments
-                                     :trigger/refinement :onyx.refinements/accumulating
-                                     :trigger/fire-all-extents? true
-                                     :trigger/id :collect-trigger
-                                     :trigger/on :onyx.triggers/segment
-                                     :trigger/threshold [1 :elements]
-                                     :trigger/sync ::update-atom!}]
+                         :lifecycles []
+                         :windows []
+                         :triggers []
                          :flow-conditions []
                          :task-scheduler :onyx.task-scheduler/balanced})]
     (-> base-job
         (add-task (consumer :read-messages
                             (merge {:kinesis/stream-name stream-name
+                                    ;:kinesis/shard-initialize-type :trim-horizon
                                     :kinesis/shard-initialize-type :latest
                                     :kinesis/deserializer-fn :onyx.tasks.kinesis/deserialize-message-edn
                                     :kinesis/region region
@@ -75,21 +51,19 @@
         client (onyx.plugin.kinesis/new-client {:kinesis/region "us-west-2"})
         job (build-job stream-name region 1 1000)
         {:keys [out read-messages]} (get-core-async-channels job)]
-    (reset! test-state nil)
-    (with-test-env [test-env [(+ n-partitions 2) env-config peer-config]]
-      ;; Give it some time to initialize to latest
+      (with-test-env [test-env [(+ n-partitions 2) env-config peer-config]]
+        ;; Give it some time to initialize to latest
 
-      (onyx.test-helper/validate-enough-peers! test-env job)
-      (let [job-id (:job-id (onyx.api/submit-job peer-config job))]
-        (Thread/sleep 4000)
-        (.putRecords client 
-                     (onyx.plugin.kinesis/build-put-request stream-name 
-                                                            segments 
-                                                            onyx.tasks.kinesis/serialize-message-edn))
-        (println "Taking segments")
-        ;(onyx.test-helper/feedback-exception! peer-config job-id)
-        (let [results (onyx.plugin.core-async/take-segments! out 45000)] 
-          (is (= (set (mapv :data segments)) (set (mapv :data results)))))
-        (is (= (mapv :data segments) (sort-by :n (mapv :data @test-state))))
-        (println "Done taking segments")
-        (onyx.api/kill-job peer-config job-id)))))
+        (onyx.test-helper/validate-enough-peers! test-env job)
+        (let [job-id (:job-id (onyx.api/submit-job peer-config job))]
+          (Thread/sleep 4000)
+          (.putRecords client 
+                       (onyx.plugin.kinesis/build-put-request stream-name 
+                                                              segments 
+                                                              onyx.tasks.kinesis/serialize-message-edn))
+          (println "Taking segments")
+          ;(onyx.test-helper/feedback-exception! peer-config job-id)
+          (let [results (onyx.plugin.core-async/take-segments! out 20000)] 
+            (is (= (sort-by :n (mapv :data segments)) (sort-by :n (mapv :data results)))))
+          (println "Done taking segments")
+          (onyx.api/kill-job peer-config job-id)))))
