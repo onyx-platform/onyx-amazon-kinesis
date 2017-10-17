53b274d16aa241cb8211e134cce1e1094e8bc4e7
OnyxBot <michael.drogalis@onyxplatform.org>
2017-10-17 03:07:30 +0000

    Update dependency org.onyxplatform/onyx to version 0.11.2-20171017_025934-gc9a8adc.
    Automatic commit by lein-unison.

--- project.clj
+++ project.clj
@@ -13,7 +13,7 @@
                              :sign-releases false}}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
-                 [org.onyxplatform/onyx "0.11.1"]
+                 [org.onyxplatform/onyx "0.11.2-20171017_025934-gc9a8adc"]
                  [com.amazonaws/amazon-kinesis-client "1.7.5"]
                  [cheshire "5.7.0"]]
   :profiles {:dev {:dependencies [[aero "0.2.0"]
