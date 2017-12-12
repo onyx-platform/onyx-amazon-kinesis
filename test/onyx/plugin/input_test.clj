12cf0066a55e760ddc0db440fe0448f56bf7b941
Lucas Bradstreet <lucasbradstreet@gmail.com>
2017-12-12 11:09:23 -0800

    Fix tests, create new shard on each run.

--- test/onyx/plugin/input_test.clj
+++ test/onyx/plugin/input_test.clj
@@ -8,6 +8,7 @@
             [onyx.plugin.core-async :refer [get-core-async-channels]]
             [onyx.plugin.test-utils :as test-utils]
             [onyx.plugin.kinesis]
+            [onyx.kinesis.utils :refer [create-stream delete-stream client-builder]]
             [onyx.api]))
 
 (def n-partitions 2)
@@ -37,33 +38,47 @@
                                    batch-settings)))
         (add-task (core-async/output :out batch-settings)))))
 
-(deftest kinesis-input-test
-  (let [{:keys [test-config env-config peer-config]} (onyx.plugin.test-utils/read-config)
-        tenancy-id (str (java.util.UUID/randomUUID)) 
-        env-config (assoc env-config :onyx/tenancy-id tenancy-id)
-        peer-config (assoc peer-config :onyx/tenancy-id tenancy-id)
-        zk-address (get-in peer-config [:zookeeper/address])
-        stream-name "ulul"
-        region "us-west-2"
-        segments (mapv (fn [n] {:partition-key (rand-int 200)
-                                :data {:n n}})
-                       (range 100))
-        client (onyx.plugin.kinesis/new-client {:kinesis/region "us-west-2"})
-        job (build-job stream-name region 1 1000)
-        {:keys [out read-messages]} (get-core-async-channels job)]
-      (with-test-env [test-env [(+ n-partitions 2) env-config peer-config]]
-        ;; Give it some time to initialize to latest
+(def stream-name "ulul")
 
-        (onyx.test-helper/validate-enough-peers! test-env job)
-        (let [job-id (:job-id (onyx.api/submit-job peer-config job))]
-          (Thread/sleep 4000)
-          (.putRecords client 
-                       (onyx.plugin.kinesis/build-put-request stream-name 
-                                                              segments 
-                                                              onyx.tasks.kinesis/serialize-message-edn))
-          (println "Taking segments")
-          ;(onyx.test-helper/feedback-exception! peer-config job-id)
-          (let [results (onyx.plugin.core-async/take-segments! out 20000)] 
-            (is (= (sort-by :n (mapv :data segments)) (sort-by :n (mapv :data results)))))
-          (println "Done taking segments")
-          (onyx.api/kill-job peer-config job-id)))))
+(deftest kinesis-input-test
+  (let [client (onyx.plugin.kinesis/new-client {:kinesis/region "us-west-2"})
+        _ (try
+           (delete-stream client stream-name)
+           ;; stream already existed (bad), give it some time to delete.
+           ;; FIXME: poll until deleted
+           (Thread/sleep 60000)
+           (catch Exception _))
+        _ (create-stream client stream-name n-partitions)
+        ;; FIXME: poll until created
+        _ (Thread/sleep 60000)]
+    (try 
+     (let [{:keys [test-config env-config peer-config]} (onyx.plugin.test-utils/read-config)
+           tenancy-id (str (java.util.UUID/randomUUID)) 
+           env-config (assoc env-config :onyx/tenancy-id tenancy-id)
+           peer-config (assoc peer-config :onyx/tenancy-id tenancy-id)
+           zk-address (get-in peer-config [:zookeeper/address])
+           region "us-west-2"
+           segments (mapv (fn [n] {:partition-key (rand-int 200)
+                                   :data {:n n}})
+                          (range 100))
+           job (build-job stream-name region 1 1000)
+           {:keys [out read-messages]} (get-core-async-channels job)]
+       (with-test-env [test-env [(+ n-partitions 2) env-config peer-config]]
+         ;; Give it some time to initialize to latest
+
+         (onyx.test-helper/validate-enough-peers! test-env job)
+         (let [job-id (:job-id (onyx.api/submit-job peer-config job))]
+           (Thread/sleep 4000)
+           (.putRecords client 
+                        (onyx.plugin.kinesis/build-put-request stream-name 
+                                                               segments 
+                                                               onyx.tasks.kinesis/serialize-message-edn))
+           (println "Taking segments")
+           ;(onyx.test-helper/feedback-exception! peer-config job-id)
+           (let [results (onyx.plugin.core-async/take-segments! out 20000)] 
+             (is (= (sort-by :n (mapv :data segments)) (sort-by :n (mapv :data results)))))
+           (println "Done taking segments")
+           (onyx.api/kill-job peer-config job-id))))
+     (finally
+      ;; FIXME: poll until deleted
+      (delete-stream client stream-name)))))
