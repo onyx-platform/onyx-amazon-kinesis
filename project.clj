4f578366d157b785e6cdb3ef4dc4cd3fe95ea4c5
Lucas Bradstreet <lucas@confluent.io>
2019-04-21 11:11:57 -0700

    Prepare for next release cycle.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.onyxplatform/onyx-amazon-kinesis "0.14.1.1"
+(defproject org.onyxplatform/onyx-amazon-kinesis "0.14.1.2-SNAPSHOT"
   :description "Onyx plugin for Amazon Kinesis"
   :url "https://github.com/onyx-platform/onyx-amazon-kinesis"
   :license {:name "Eclipse Public License"
